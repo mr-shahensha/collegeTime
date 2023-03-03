1023573decb5595310cbb3c9dd9935e9684d2735
shahensha <64788000+mr-shahensha@users.noreply.github.com>
2023-03-04 00:17:01 +0530

    home page done

--- home.php
+++ home.php
@@ -4,10 +4,75 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>collegeTime</title>
 </head>
 <body>
-    <h1>collegetime</h1>
-    <a href="login.php">login</a>
+    <!-- This is header -->
+    <Header>
+        <div class="logo"><img src="assets/logo.jpg" alt="logo" style="height:100px;width:150px;"></div>
+        <nav>
+            <ul>
+                <li><a href="">Course</a></li>
+                <li><a href="">Colleges</a></li>
+                <li><a href="">Statistics</a></li>
+                <li><a href="">Blogs</a></li>
+                <li><a href="login.php">Login</a></li>
+            </ul>
+        </nav>
+    </Header>
+    <!-- Main section start here -->
+    <Section class="main">
+        <div class="main-image">
+            <h1>Best College in india</h1>
+            <h2>Chose your favourite course today</h2>
+            <p>goto join page</p>
+        </div>
+    </Section>
+    <section class="second">
+            <img src="" alt="" class="second-legecy-image">
+            <div class="legecy-comment">
+                <h1>Legecy that we are caring for 105years</h1>
+                <p>And we promise we will carry forever</p>
+            </div>
+    </section>
+    <section class="news">
+        <div class="news-post">
+            <img src="" alt="">
+            <h1>This is a header</h1>
+            <p>date-time</p>
+            <button>learn more</button>
+        </div>
+    </section>
+    <section class="footer">
+        <div class="pages">
+            <h1>navigation bar</h1>
+            <ul>
+                <li><a href="">Course</a></li>
+                <li><a href="">Colleges</a></li>
+                <li><a href="">Statistics</a></li>
+                <li><a href="">Blogs</a></li>
+                <li><a href="login.php">Login</a></li>
+            </ul>
+        </div>
+        <div class="link">
+            <h1>College links</h1>
+            <ul>
+                <li><a href="">facebook</a></li>
+                <li><a href="">instagram</a></li>
+                <li><a href="">youtube</a></li>
+            </ul>
+        </div>
+        <div class="contacts">
+            <h1>Contact us</h1>
+            <ul>
+                <li><a href="">987654321</a></li>
+                <li><a href="">collegetime@fakeemail.com</a></li>
+                <li><a href="">collegetime.com</a></li>
+            </ul>
+        </div>
+    </section>
+    <section class="end">
+        <h1>this website is created by <span>Shahensha</span></h1>
+    </section>
 </body>
 </html>
