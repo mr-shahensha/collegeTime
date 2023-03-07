56183a65ff1e14322260abf3ced226fa691206a4
shahensha <64788000+mr-shahensha@users.noreply.github.com>
2023-03-08 01:05:17 +0530

    little bit done

---
+++ footer.php
@@ -0,0 +1,37 @@
+  <link rel="stylesheet" href="css/home.css">
+  <!-- footer section start -->
+  <section class="footer">
+        <!-- navigation links -->
+        <div class="pages">
+            <h1>navigation bar</h1>
+            <ul>
+                <li><a href="">Course</a></li>
+                <li><a href="">Colleges</a></li>
+                <li><a href="">Statistics</a></li>
+                <li><a href="">Blogs</a></li>
+                <li><a href="login.php">Login</a></li>
+            </ul>
+        </div>
+        <!-- web links of collegetime -->
+        <div class="link">
+            <h1>College links</h1>
+            <ul>
+                <li><a href="">facebook</a></li>
+                <li><a href="">instagram</a></li>
+                <li><a href="">youtube</a></li>
+            </ul>
+        </div>
+        <!-- contacts -->
+        <div class="contacts">
+            <h1>Contact us</h1>
+            <ul>
+                <li><a href="">987654321</a></li>
+                <li><a href="">collegetime@fakeemail.com</a></li>
+                <li><a href="">collegetime.com</a></li>
+            </ul>
+        </div>
+    </section>
+    <!-- finally me :) -->
+    <section class="end">
+        <h1 >this website is created by <a style="color:rgba(0, 0, 0, 0.755);" href="https:/shahensha.tech" target="_blank">Shahensha</a></h1>
+    </section>
--- login.php
+++ login.php
@@ -1,54 +1,61 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<script type="text/javascript" src="jquery-1.6.4.min.js"></script>
-    <title>Document</title>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <!-- javascript link -->
+    <script type="text/javascript" src="jquery-1.6.4.min.js"></script>
+    <!-- css link -->
+    <link rel="stylesheet" href="css/login.css">
+        <title>Login Page</title>
+    </head>
+    <body>
+        <!-- this is header -->
+    <Header>
+            <div class="logo"><img src="assets/logo.jpg" alt="logo" ></div>
+        </header>
+        <!-- this is hero section -->
+        <section class="hero">
+            <div class="img">
+            <img src="assets/students.png" alt="image">
+            </div>
+            <div class="login-form">
+            <form action="logic/loginlogic.php" method="post" onsubmit="return validation()">
+            <input type="text" placeholder="enter id" name="id" id='id'  onkeyup="return hidewarn(this.value)">
+
+            <input type="text" placeholder="enter password"  name="pass" id='pass' onkeyup="return hidewarn2(this.value)">
+              
+            <input type="submit" value="submit" name="submit">
+            </div>
+            <b><p id="vali1" style="color:red;"></p></b>
+            <b><p id="vali2" style="color:red;"></p></b>
+        </form>
+            
+    </section>
+        <?php include("footer.php");?>
+    </body>
+    </html>
     <script>
-        function validation(){
-            var id = document.getElementById('id').value;
-            if(id<2){
-                text="Please enter valid ID !!";
-                document.getElementById("vali1").innerHTML=text;
-                return false;
+            function validation(){
+                var id = document.getElementById('id').value;
+                if(id<2){
+                    text="Please enter valid ID !!";
+                    document.getElementById("vali1").innerHTML=text;
+                    return false;
+                }
+                var pass = document.getElementById('pass').value;
+                if(pass<2){
+                    text="Please enter valid Password !!";
+                    document.getElementById("vali2").innerHTML=text;
+                    return false;
+                }
             }
-            var pass = document.getElementById('pass').value;
-            if(pass<2){
-                text="Please enter valid Password !!";
-                document.getElementById("vali2").innerHTML=text;
-                return false;
+            function hidewarn(a){
+                if(a!=''){
+                    document.getElementById("vali1").innerHTML="";
+                }
             }
-        }
-        function hidewarn(a){
-            if(a!=''){
-                document.getElementById("vali1").innerHTML="";
+            function hidewarn2(a){
+                if(a!=''){
+                    document.getElementById("vali2").innerHTML="";
+                }
             }
-        }
-        function hidewarn2(a){
-            if(a!=''){
-                document.getElementById("vali2").innerHTML="";
-            }
-        }
-    </script>
-</head>
-<body>
-    <form action="logic/loginlogic.php" method="post" onsubmit="return validation()">
-        <table border='1'>
-            <tr>
-                <td>
-                <input type="text" placeholder="enter id" name="id" id='id'  onkeyup="return hidewarn(this.value)"><br>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                <input type="text" placeholder="enter password"  name="pass" id='pass' onkeyup="return hidewarn2(this.value)">
-                </td>
-                <td>
-                <input type="submit" value="submit" name="submit">
-                </td>
-            </tr>
-        </table>
-        <b><p id="vali1" style="color:red;"></p></b>
-        <b><p id="vali2" style="color:red;"></p></b>
-    </form>
-</body>
-</html>
+        </script>
