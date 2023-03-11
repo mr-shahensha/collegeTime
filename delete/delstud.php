9597068aeae107f43a3ac6cea5d1b634e3ba1fa7
shahensha <64788000+mr-shahensha@users.noreply.github.com>
2023-03-11 11:00:31 +0530

    Update delstud.php

--- delete/delstud.php
+++ delete/delstud.php
@@ -4,6 +4,9 @@ include("../logic/connection.php");
 // $idd=$_SESSION['id'];
 $sid=$_REQUEST['sid'];
 $query=mysqli_query($con,"DELETE FROM student WHERE `sid` = '$sid'")or die(mysqli_error());
+$query1=mysqli_query($con,"DELETE FROM signup WHERE `id` = '$sid'")or die(mysqli_error());
+// $query2=mysqli_query($con,"DELETE FROM student WHERE `stud_id` = '$sid'")or die(mysqli_error());
+
 ?>
 <script>
     alert("sucessfully deleted ");
