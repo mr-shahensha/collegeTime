61ce86eee1175020d1cfa58576059132e822e7be
shahensha <64788000+mr-shahensha@users.noreply.github.com>
2023-03-22 22:30:56 +0530

    basic all done

--- notification.php
+++ notification.php
@@ -8,7 +8,15 @@ $idd=$_SESSION['id'];
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Document</title>
+<link rel="stylesheet" href="css/index.css">
+<link rel="stylesheet" href="css/degree.css">
+    <!-- fontawsome link -->
+    <script src="https://kit.fontawesome.com/dce4936410.js" crossorigin="anonymous"></script>
+     <!-- google font -->
+     <style>
+        @import url('https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap');
+    </style>
+    <title>Notification</title>
     <script>
         function status(stat,stud,sid){
             if(confirm('are you sure ?')){
@@ -18,9 +26,19 @@ $idd=$_SESSION['id'];
     </script>
 </head>
 <body>
-<a href="index.php"><-home</a>
-<br><br>
-    <table border="2">
+    <!-- this is header -->
+    <Header>
+        <div class="logo"><img src="assets/logo.jpg" alt="logo" ></div>
+    </header>
+ <!-- this is hello section -->
+<section class="hello">
+<h1>Notification page</h1>
+</section>
+<!-- this is hero section -->
+<section class="degree-hero">
+    <!-- hero section part one -->
+    <div class="part-one">
+    <table>
         <tr>
             <td>
                <b>Your notifications</b> 
@@ -81,5 +99,9 @@ $idd=$_SESSION['id'];
                 echo "You have no notification";
              }}?>
     </table>
+    </div>
+    </section>
+    <!-- this is footer section -->
+    <?php include("footer.php");?>
 </body>
 </html>
--- password.php
+++ password.php
@@ -2,15 +2,61 @@
  include("logic/connection.php");
  include("logic/back.php");
 $idd=$_SESSION['id'];
-
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Document</title>
+<link rel="stylesheet" href="css/index.css">
+<link rel="stylesheet" href="css/degree.css">
+    <!-- fontawsome link -->
+    <script src="https://kit.fontawesome.com/dce4936410.js" crossorigin="anonymous"></script>
+     <!-- google font -->
+     <style>
+        @import url('https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap');
+    </style>
+    <title>Change Password</title>
 </head>
 <body>
-    <script>
+      <!-- this is header -->
+      <Header>
+        <div class="logo"><img src="assets/logo.jpg" alt="logo" ></div>
+    </header>
+ <!-- this is hello section -->
+<section class="hello">
+<h1>Change password</h1>
+</section>
+<!-- this is hero section -->
+<section class="degree-hero">
+    <!-- hero section part one -->
+    <div class="part-one">
+<form action="logic/passlogic.php" method="post" onsubmit='return validation()'>
+        <table>
+            <tr>
+                <td>Enter old password</td>
+                <td><input type="password" autocomplete='off' placeholder="enter old password" name='oldpass' id='oldpass' onkeyup='return hidewarn(this.value)'></td>
+            </tr>
+            <tr>
+                <td>Enter new password</td>
+                <td><input type="password" autocomplete='off' placeholder="enter new password" name='newpass' id='newpass' onkeyup='return hidewarn1(this.value)'></td>
+            </tr> <tr>
+                <td>Confirm new password</td>
+                <td><input style="width:350px;" type="text" autocomplete='off' placeholder="confirm newpassword" name='conpass' id='conpass' onkeyup='return hidewarn2(this.value)'></td>
+            </tr>
+            <tr><td><input type="submit" value="submit" name='submit'></td></tr>
+        </table>
+        <b><p id='warn' style='color:red;'></p></b>
+        <b><p id='warn1' style='color:red;'></p></b>
+        <b><p id='warn2' style='color:red;'></p></b>
+        <b><p id='warn3' style='color:red;'></p></b>
+    </form>
+    </div>
+    </section>
+    <!-- this is footer section -->
+    <?php include("footer.php");?>
+</body>
+</html>
+<script>
         function validation(){
         var oldpass=document.getElementById('oldpass').value;
         if(oldpass.length==''){
@@ -54,29 +100,3 @@ $idd=$_SESSION['id'];
             }
         }
     </script>
-</body>
-</html>
-<a href="profile.php"><-back</a>
-    <br><br>
-
-    <form action="logic/passlogic.php" method="post" onsubmit='return validation()'>
-        <table border="2">
-            <tr>
-                <td>Enter old password</td>
-                <td><input type="password" autocomplete='off' placeholder="enter old password" name='oldpass' id='oldpass' onkeyup='return hidewarn(this.value)'></td>
-            </tr>
-            <tr>
-                <td>Enter new password</td>
-                <td><input type="password" autocomplete='off' placeholder="enter new password" name='newpass' id='newpass' onkeyup='return hidewarn1(this.value)'></td>
-            </tr> <tr>
-                <td>Confirm new password</td>
-                <td><input type="text" autocomplete='off' placeholder="confirm newpassword" name='conpass' id='conpass' onkeyup='return hidewarn2(this.value)'></td>
-            </tr>
-            <tr><td><input type="submit" value="submit" name='submit'></td></tr>
-        </table>
-        <b><p id='warn' style='color:red;'></p></b>
-        <b><p id='warn1' style='color:red;'></p></b>
-        <b><p id='warn2' style='color:red;'></p></b>
-        <b><p id='warn3' style='color:red;'></p></b>
-
-    </form>
